1bd9f7192b514d2bbcb678eee09bb81d1d5a2b17
Pierre-Yves Le Guennec <pyves.lg@gmail.com>
2017-12-01 18:53:11 +0100

    seed done from MFE github repo

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "open-uri"
+require "json"
+require "yaml"
+
+CLIENT_ID = ENV['GH_BASIC_CLIENT_ID']
+CLIENT_SECRET = ENV['GH_BASIC_SECRET_ID']
+
+url= "https://api.github.com/repos/pyveslg/MFE/contents/quizzes?client_id=#{CLIENT_ID}&client_secret=#{CLIENT_SECRET}"
+
+Answer.destroy_all
+Option.destroy_all
+Question.destroy_all
+Quiz.destroy_all
+
+
+fetch_ymls= open(url).read
+ymls = JSON.parse(fetch_ymls)
+ymls.each_with_index do |yml, i|
+  quizz_url = ymls[i]['download_url']
+  quizz_content = open(quizz_url).read
+  quizz = YAML.load(quizz_content)
+
+
+  quiz = Quiz.create!
+  quiz.slug = quizz["slug"]
+  quiz.name = quizz["name"]
+  quiz.path = quizz["path"]
+  quiz.save!
+
+  quizz["questions"].each do |question|
+      new_question = Question.create(:quiz_id => quiz.id)
+      new_question.name = question["name"]
+      new_question.save
+
+      question["options"].each do |option|
+        option["question_id"] = new_question.id
+        Option.create!(option)
+      end
+  end
+end
+puts "Finished"
