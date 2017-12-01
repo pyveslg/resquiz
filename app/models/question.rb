6a73f8a58824d9dd0a812f4cd5b6cd02c5a0ab18
Pierre-Yves Le Guennec <pyves.lg@gmail.com>
2017-12-01 19:09:57 +0100

    checking if question_id is first, last and getting id of previous/next question

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,24 @@ class Question < ApplicationRecord
   belongs_to :quiz
   has_many :options
   has_one :answer
+
+  ## RENDER FOLLOWING QUESTION ID OR NIL IF LAST
+  def next_id
+    self.class.where("id > ? and quiz_id = ?", self.id, self.quiz_id).pluck(:id).first
+  end
+
+  ## RENDER PREVIOUS QUESTION ID OR NIL IF FIRST
+  def previous_id
+    self.class.where("id < ? and quiz_id = ?", self.id, self.quiz_id).pluck(:id).last
+  end
+
+  ## CHECK IF ID IS FIRST
+  def first?
+    self.previous_id.nil?
+  end
+
+  ## CHECK IF ID IS LAST
+  def last?
+    self.next_id.nil?
+  end
 end
--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -3,6 +3,8 @@ class Quiz < ApplicationRecord
   has_many :answers, through: :questions
   has_many :options, through: :questions
 
+  ## Instance Method to check if the quiz has been played or not. Return false if not played.
+
   def played?
     !self.answers.empty?
   end
