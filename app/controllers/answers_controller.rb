021deade8eea3d79c1791fcf2d234520a2e569a6
Pierre-Yves Le Guennec <pyves.lg@gmail.com>
2017-12-02 11:39:29 +0100

    @answer created with scoring method

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,35 @@
 class AnswersController < ApplicationController
+
+
+  def create
+    @answer = Answer.new(answer_params)
+    @answer.question = Question.find(params[:question_id])
+    scoring
+    binding.pry
+    @answer.save
+    @question = @answer.question
+    redirect_to question_path(@question.next_id)
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+
+  private
+
+  def scoring
+    @selected_option = Option.find(@answer.choice_id)
+    if @selected_option.correct
+      @answer.score = 1
+    else
+      @answer.score = 0
+    end
+  end
+
+  def answer_params
+    params.require(:answer).permit(:choice_id)
+  end
 end
